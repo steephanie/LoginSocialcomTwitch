2e80b01e9533427d0cb51a75b34f7b1bbb69c730
Stephanie Nascimento <54187353+steephanie@users.noreply.github.com>
2021-11-18 14:52:53 -0300

    upload final.

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -36,66 +36,105 @@ function AuthProvider({ children }: AuthProviderData) {
   const [user, setUser] = useState({} as User);
   const [userToken, setUserToken] = useState('');
 
-  // get CLIENT_ID from environment variables
+  const { CLIENT_ID } = process.env;
 
   async function signIn() {
     try {
       // set isLoggingIn to true
+      setIsLoggingIn(true);
 
       // REDIRECT_URI - create OAuth redirect URI using makeRedirectUri() with "useProxy" option set to true
+      const REDIRECT_URI = makeRedirectUri({ useProxy: true });
+
       // RESPONSE_TYPE - set to "token"
+      const RESPONSE_TYPE = "token";
+
       // SCOPE - create a space-separated list of the following scopes: "openid", "user:read:email" and "user:read:follows"
+      const SCOPE = "openid%20user:read:email%20user:read:follows";
+
       // FORCE_VERIFY - set to true
+      const FORCE_VERIFY = true;
+
       // STATE - generate random 30-length string using generateRandom() with "size" set to 30
+      const STATE = generateRandom(30);
 
       // assemble authUrl with twitchEndpoint authorization, client_id, 
       // redirect_uri, response_type, scope, force_verify and state
+      const authUrl = twitchEndpoints.authorization +
+        `?client_id=${CLIENT_ID}` +
+        `&redirect_uri=${REDIRECT_URI}` +
+        `&response_type=${RESPONSE_TYPE}` +
+        `&scope=${SCOPE}` +
+        `&force_verify=${FORCE_VERIFY}` +
+        `&state=${STATE}`;
 
       // call startAsync with authUrl
+      const authResponse = await startAsync({ authUrl });
 
       // verify if startAsync response.type equals "success" and response.params.error differs from "access_denied"
       // if true, do the following:
 
+      if (authResponse.type === 'success' && authResponse.params.error !== 'access_denied') {
         // verify if startAsync response.params.state differs from STATE
         // if true, do the following:
-          // throw an error with message "Invalid state value"
+        // throw an error with message "Invalid state value"
+        if (authResponse.params.state !== STATE) {
+          throw new Error("Invalid state value");
+        }
 
         // add access_token to request's authorization header
+        api.defaults.headers.authorization = `Bearer ${authResponse.params.access_token}`;
 
         // call Twitch API's users route
-
         // set user state with response from Twitch API's route "/users"
         // set userToken state with response's access_token from startAsync
+        const userResponse = await api.get('/users');
+        setUser(userResponse.data.data[0]);
+        setUserToken(authResponse.params.access_token);
+        console.log(userResponse);
+      }
     } catch (error) {
       // throw an error
+      throw new Error;
     } finally {
       // set isLoggingIn to false
+      setIsLoggingIn(false);
     }
   }
 
   async function signOut() {
     try {
       // set isLoggingOut to true
+      setIsLoggingOut(true);
 
       // call revokeAsync with access_token, client_id and twitchEndpoint revocation
+      await api.post(twitchEndpoints.revocation, {
+        token: userToken,
+        clientId: CLIENT_ID
+      })
     } catch (error) {
     } finally {
       // set user state to an empty User object
       // set userToken state to an empty string
+      setUser({} as User);
+      setUserToken('');
 
       // remove "access_token" from request's authorization header
+      delete api.defaults.headers.authorization;
 
       // set isLoggingOut to false
+      setIsLoggingOut(false);
     }
   }
 
   useEffect(() => {
     // add client_id to request's "Client-Id" header
+    api.defaults.headers['Client-Id'] = CLIENT_ID;
   }, [])
 
   return (
     <AuthContext.Provider value={{ user, isLoggingOut, isLoggingIn, signIn, signOut }}>
-      { children }
+      {children}
     </AuthContext.Provider>
   )
 }
